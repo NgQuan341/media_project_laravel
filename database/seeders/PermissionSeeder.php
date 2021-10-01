01b3628394b03a28c17b2158e6a6f20082418a54
NguyenQuan <nghongquan34@gmail.com>
2021-10-01 16:09:34 +0700

    first commit

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -10,22 +10,34 @@ class PermissionSeeder extends Seeder
     public function run()
     {
         DB::table('permissions')->insert([
+            ['name' => 'view_user'],
+            ['name' => 'create_user'],
+            ['name' => 'update_user'],
+            ['name' => 'delete_user'],
+
+            ['name' => 'view_unit'],
+            ['name' => 'update_unit'],
+
+            ['name' => 'view_all_branch'],
+            ['name' => 'view_branch'],
+            ['name' => 'create_branch'],
+            ['name' => 'update_branch'],
+            ['name' => 'delete_branch'],
+
+            ['name' => 'view_department'],
+            ['name' => 'create_department'],
+            ['name' => 'update_department'],
+            ['name' => 'delete_department'],
+
             ['name' => 'view_content'],
             ['name' => 'create_content'],
             ['name' => 'update_content'],
             ['name' => 'delete_content'],
             ['name' => 'download_content'],
             
-            ['name' => 'view_user'],
-            ['name' => 'create_user'],
-            ['name' => 'update_user'],
-            ['name' => 'delete_user'],
-            ['name' => 'change_role'],
+            
 
-            ['name' => 'view_department'],
-            ['name' => 'create_department'],
-            ['name' => 'update_department'],
-            ['name' => 'delete_department'],
+           
         ]);
     }
 }
